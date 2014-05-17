227c13f112d03dff3159919fc989c332f6289c04
alebaffa <alessandro.baffa@gmail.com>
2014-05-17 22:35:54 +0200

    Problem One Java commit

---
+++ ProblemOne.java
@@ -0,0 +1,23 @@
+package com.euler.project;
+
+/**
+ * Problem 1
+ * 
+ * Description: If we list all the natural numbers below 10 that are multiples
+ * of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the
+ * sum of all the multiples of 3 or 5 below 1000.
+ * 
+ * Expected result: 233168
+ * */
+public class ProblemOne {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 3; i < 1000; i++) {
+			if (((i % 3) == 0) || (i % 5) == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("Final sum: " + sum);
+	}
+}
